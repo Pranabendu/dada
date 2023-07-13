081f6774ef044131dd451ddb01d838407793b67d
Pranabendu <pranabendugiri@gmail.com>
2023-07-13 10:40:54 +0530

    BandaBia

---
+++ BandaBia/src/test/java/biakha/ToGandireGiha.java
@@ -0,0 +1,14 @@
+package biakha;
+
+import org.testng.annotations.Test;
+
+public class ToGandireGiha {
+	
+	@Test
+	
+	public void magia()
+	{
+		System.out.println("Banda ku chodi hauchu bengali");
+	}
+
+}
