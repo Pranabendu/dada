901c82d860dd1aa81725e6550754592e474e373e
Pranabendu <pranabendugiri@gmail.com>
2023-09-06 10:41:06 +0530

    commit

---
+++ BandaBia/src/test/java/Mock/Ok.java
@@ -0,0 +1,27 @@
+package Mock;
+
+public class Ok {
+	public static void main(String[] args) {
+//		int [] arr = {7, -6, 11, 9, 0, 4};
+//		int fstmin = arr[1];
+//		int sndmin = arr[2];
+//		int thrdmin = arr[3];
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i]<fstmin) {
+//				fstmin = arr[i];
+//			}
+//			if(fstmin<arr[i]) {
+//				sndmin = arr[i];
+//			}
+//		}
+//		System.out.println(fstmin);
+//		System.out.println(sndmin);
+//		System.out.println(thrdmin);
+		
+		int [] arr = {010, 011, 12, 013, 14};
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+	
+}
